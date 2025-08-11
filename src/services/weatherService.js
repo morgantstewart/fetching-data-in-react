0c5f368e86ff8f588339752887aaef521e9239cd
morgantstewart <morgan@paysigil.com>
2025-08-11 10:51:19 -0400

    Implemented Weather API

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,11 +1,11 @@
 
 const API_KEY = 'd0611594777b41f0bce143220251108';
-const BASE_URL = `http://api.weatherapi.com/v1/current.json?key=${d0611594777b41f0bce143220251108}`;
+const BASE_URL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}`;
 
 
-const show = async (city) => {
+const show = async (Atlanta) => {
   try {
-    const queryString = `&q=${city}`;
+    const queryString = `&q=${Atlanta}`;
     const res = await fetch(BASE_URL + queryString);
     const data = await res.json();
     console.log('Data:', data);
@@ -14,3 +14,8 @@ const show = async (city) => {
     console.log(err);
   }
 };
+
+
+
+
+export { show };
