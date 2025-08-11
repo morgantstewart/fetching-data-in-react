2b5b77428a9089a9bab3a70caa6cbfce25dc980e
morgantstewart <morgan@paysigil.com>
2025-08-11 14:49:09 -0400

    No errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,42 @@
 import * as weatherService from './services/weatherService';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import WeatherDetails from './components/WeatherDetails/WeatherDetails';
 import WeatherSearch from './components/WeatherSearch/WeatherSearch';
-
-const [weather, setWeather] = useState({});
-
-
-const newWeatherState = {
-  location: data.location.name,
-  temperature: data.current.temp_f,
-  condition: data.current.condition.text,
-};
-
-
-
-
 
 const App = () => {
   const [weather, setWeather] = useState({});
 
-
-  const fetchData = async (Atlanta) => {
-    const data = await weatherService.show(Atlanta);
+  const fetchData = async (city) => {
+    const data = await weatherService.show(city);
     const newWeatherState = {
       location: data.location.name,
       temperature: data.current.temp_f,
       condition: data.current.condition.text,
     };
     setWeather(newWeatherState);
-f26764c16dd758357dfae5f321ba5726c1ac5ca2
   };
-  setWeather(newWeatherState);
-  
+
+  useEffect(() => {
+    const fetchDefaultData = async () => {
+      const data = await weatherService.show('Atlanta');
+      const newWeatherState = {
+        location: data.location.name,
+        temperature: data.current.temp_f,
+        condition: data.current.condition.text,
+      };
+      setWeather(newWeatherState);
+    };
+
+    fetchDefaultData();
+  }, []);
 
   return (
     <main>
       <h1>Weather API</h1>
-      <WeatherSearch onSearch={fetchData} />
+      <WeatherSearch fetchData={fetchData} />
       <WeatherDetails weather={weather} />
     </main>
   );
 };
-
-
-
-
-console.log('State:', weather);
 
 export default App;
